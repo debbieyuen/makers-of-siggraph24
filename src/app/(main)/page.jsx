3010b9ad7731580d03d81fe685dff240a90cd6d5
Debbie Yuen <deborahyuen2016@berkeley.edu>
2024-07-06 14:52:24 -0700

    edit page layout

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -8,9 +8,9 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <Speakers />
+      {/* <Speakers /> */}
       <Schedule />
-      <Sponsors />
+      {/* <Sponsors /> */}
       <Newsletter />
     </>
   )
