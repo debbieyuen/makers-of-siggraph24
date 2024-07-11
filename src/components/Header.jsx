1cb03d2b66b272ed347e31544190e5104409c7f7
Debbie Yuen <deborahyuen2016@berkeley.edu>
2024-07-11 15:05:44 -0700

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,20 +8,22 @@ export function Header() {
     <header className="relative z-50 flex-none lg:pt-11">
       <Container className="flex flex-wrap items-center justify-center sm:justify-between lg:flex-nowrap">
         <div className="mt-10 lg:mt-0 lg:grow lg:basis-0">
-          <Logo className="h-12 w-auto text-slate-900" />
+          {/* <Logo className="h-12 w-auto text-slate-900" /> */}
         </div>
         <div className="order-first -mx-4 flex flex-auto basis-full overflow-x-auto whitespace-nowrap border-b border-blue-600/10 py-4 font-mono text-sm text-blue-600 sm:-mx-6 lg:order-none lg:mx-0 lg:basis-auto lg:border-0 lg:py-0">
           <div className="mx-auto flex items-center gap-4 px-4">
+            <p>SIGGRAPH 2024</p>
+            <DiamondIcon className="h-1.5 w-1.5 overflow-visible fill-current stroke-current" />
             <p>
-              <time dateTime="2022-04-04">04</time>-
-              <time dateTime="2022-04-06">06 of April, 2022</time>
+              {/* <time dateTime="2022-30-0">04</time>- */}
+              <time dateTime="2024-30-04">July 30th 2024</time>
             </p>
             <DiamondIcon className="h-1.5 w-1.5 overflow-visible fill-current stroke-current" />
             <p>Denver, CO</p>
           </div>
         </div>
         <div className="hidden sm:mt-10 sm:flex lg:mt-0 lg:grow lg:basis-0 lg:justify-end">
-          <Button href="#">Get your tickets</Button>
+          <Button href="https://s2024.conference-program.org/presentation/?id=gensub_250&sess=sess268">Register</Button>
         </div>
       </Container>
     </header>
